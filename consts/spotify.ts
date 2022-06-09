d9dcc5a7ffc10dbbe0064ed913ff96b1d75109ef
Paul Mineev <paul@mineev.me>
2022-06-09 10:07:22 -0700

    log undefined environment variables

--- consts/spotify.ts
+++ consts/spotify.ts
@@ -1,7 +1,12 @@
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REFRESH_TOKEN } = process.env;
 
-export const CLIENT_ID = SPOTIFY_CLIENT_ID;
-export const CLIENT_SECRET = SPOTIFY_CLIENT_SECRET;
-export const REFRESH_TOKEN = SPOTIFY_REFRESH_TOKEN;
+Object.entries({ SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REFRESH_TOKEN }).forEach(
+	(key) => {
+		console.error(`Missing environment variable: ${key}`);
+	}
+);
+export const CLIENT_ID = SPOTIFY_CLIENT_ID ?? "";
+export const CLIENT_SECRET = SPOTIFY_CLIENT_SECRET ?? "";
+export const REFRESH_TOKEN = SPOTIFY_REFRESH_TOKEN ?? "";
 export const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
 export const NOW_PLAYING_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing";
